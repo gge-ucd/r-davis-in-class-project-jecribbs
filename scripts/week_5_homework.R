fb614f0c6be9750c70df06f3cb80207282132015
jecribbs <jecribbs@ucdavis.edu>
2021-10-25 21:35:51 -0700

    Homework 5 with corrections and comments from the suggested answers.

--- scripts/week_5_homework.R
+++ scripts/week_5_homework.R
@@ -8,6 +8,14 @@ surveys <- read_csv('data/portal_data_joined.csv')
 surveys_wide <- pivot_wider(surveys, id_cols = genus, names_from = plot_type, values_from = hindfoot_length, values_fn = mean) # this seems to work, but generates a lot of NAs...is this right? 
 # I tried piping to sort and adding names_sort as an argument to pivot_wider, but I don't think I understand how to sort these results %>% sort(surveys_wide$Control)  
 
+# suggested answer
+surveys_wide <- surveys %>% 
+  filter(!is.na(hindfoot_length)) %>% # removing the NAs for hindfoot length 
+  group_by(genus, plot_type) %>% 
+  summarise(mean_hindfoot = mean(hindfoot_length)) %>% # Is this the same as values_from = hindfoot_length, values_fn = mean or does the latter not really work?
+  pivot_wider(names_from = plot_type, values_from = mean_hindfoot) %>% 
+  arrange(Control) # arrange sorts by the Control column
+
 #2. Using the original surveys dataframe, use the two different functions we laid out for conditional statements, ifelse() and case_when(), to calculate a new weight category variable called weight_cat. For this variable, define the rodent weight into three categories, where “small” is less than or equal to the 1st quartile of weight distribution, “medium” is between (but not inclusive) the 1st and 3rd quartile, and “large” is any weight greater than or equal to the 3rd quartile. (Hint: the summary() function on a column summarizes the distribution). For ifelse() and case_when(), compare what happens to the weight values of NA, depending on how you specify your arguments.
 
 # use summary to find the quartiles
@@ -27,18 +35,19 @@ surveys_casewhen <- surveys %>% mutate(weight_cat = case_when(weight <= 20.00 ~ 
 # BONUS: How might you soft code the values (i.e. not type them in manually) of the 1st and 3rd quartile into your conditional statements in question 2? 
 
 # returns the first quantile value (2nd element of the summary)
-summary(surveys$weight)[2]
+summary(surveys$weight)[2] # returns 1st Qu. 20
 # returns the third quantile value (5th element of the summary)
-summary(surveys$weight)[5]
+summary(surveys$weight)[[5]] # returns only the number 48
 
 # using indexing as shown above to soft code the first and third quantiles
-surveys_ifelse_softcode <- na.omit(surveys) %>% mutate(weight_cat = ifelse(weight <= summary(surveys$weight)[2], "small", ifelse(weight > summary(surveys$weight)[5], "large", "medium")))
+surveys_ifelse_softcode <- na.omit(surveys) %>% mutate(weight_cat = ifelse(weight <= summary(surveys$weight)[[2]], "small", ifelse(weight > summary(surveys$weight)[[5]], "large", "medium")))
 
 # saving summary results as weight summary to make all the cases less ugly
 weight_summary <- summary(surveys$weight)
 # using indexing on the weight summary object
-surveys_casewhen_softcode <- surveys %>% mutate(weight_cat = case_when(weight <= weight_summary[2] ~ "small", weight > weight_summary[2] & weight < weight_summary[5] ~ "medium", weight >= weight_summary[5] ~ "large", is.na(weight) ~ "no weight"))
+surveys_casewhen_softcode <- surveys %>% mutate(weight_cat = case_when(weight <= weight_summary[[2]] ~ "small", weight > weight_summary[[2]] & weight < weight_summary[[5]] ~ "medium", weight >= weight_summary[[5]] ~ "large", is.na(weight) ~ "no weight"))
 
 # double check with identical to make sure the hard code and soft code results are the same
 identical(surveys_casewhen, surveys_casewhen_softcode) # TRUE
 identical(surveys_ifelse, surveys_ifelse_softcode) # TRUE
+# This checked out with single brackets too, but it seems better to extract the numeric values only from the summary table. 
