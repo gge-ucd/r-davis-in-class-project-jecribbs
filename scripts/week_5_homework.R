355c1282a48d545cbe149130f361c69392a19641
jecribbs <jecribbs@ucdavis.edu>
2021-12-01 22:42:16 -0800

    Homework 5 update, I think

--- scripts/week_5_homework.R
+++ scripts/week_5_homework.R
@@ -5,6 +5,7 @@
 
 library(tidyverse)
 surveys <- read_csv('data/portal_data_joined.csv')
+
 surveys_wide <- pivot_wider(surveys, id_cols = genus, names_from = plot_type, values_from = hindfoot_length, values_fn = mean) # this seems to work, but generates a lot of NAs...is this right? 
 # I tried piping to sort and adding names_sort as an argument to pivot_wider, but I don't think I understand how to sort these results %>% sort(surveys_wide$Control)  
 
@@ -51,3 +52,69 @@ surveys_casewhen_softcode <- surveys %>% mutate(weight_cat = case_when(weight <=
 identical(surveys_casewhen, surveys_casewhen_softcode) # TRUE
 identical(surveys_ifelse, surveys_ifelse_softcode) # TRUE
 # This checked out with single brackets too, but it seems better to extract the numeric values only from the summary table. 
+
+########
+library(tidyverse)
+surveys <- read_csv("data/portal_data_joined.csv")
+
+# EXPLORING the NA situation with conditional statements
+## leaving the last "else" argument in case_when assigns the 'large' value to EVERYTHING else, including NAs
+
+surveys %>%
+  mutate(weight_cat = case_when(
+    weight >= 20.00 ~ "small",
+    weight > 20.00 & weight < 48.00 ~ "medium",
+    T ~ "large"
+  )) %>%
+  select(weight, weight_cat) %>%
+  filter(is.na(weight))
+
+
+# leaving the last "else" argument in ifelse assigns the 'large' value to everything else, BUT DOES NOT INCLUDE NAs
+surveys %>%
+  mutate(weight_cat = ifelse(weight >= 20.00, "small",
+                             ifelse(weight > 20.00 & weight < 48.00, "medium"
+                                    ,"large"))) %>%
+  select(weight, weight_cat) %>%
+  filter(is.na(weight))
+
+# specify the final argument in case_when()
+surveys %>%
+  mutate(weight_cat = case_when(
+    weight >= 20.00 ~ "small",
+    weight > 20.00 & weight < 48.00 ~ "medium",
+    weight >= 48.00 ~ "large"
+  )) %>%
+  select(weight, weight_cat) %>%
+  filter(is.na(weight))
+
+# Manipulate surveys to create a new dataframe called surveys_wide with:
+# 1. column for genus and a column named after every plot type (step 2)
+# 2. each of these columns containing the mean hindfoot length of animals in that plot type and genus. So every row has a genus and then a mean hindfoot length value for every plot type. (step 1)
+# 3. The dataframe should be sorted by values in the Control plot type column. (step 3)
+
+# Step 1:
+surveys2 <- surveys %>%
+  filter(!is.na(hindfoot_length)) %>%
+  group_by(plot_type, genus) %>%
+  summarize(mean_hindfoot = mean(hindfoot_length))
+surveys2
+
+# Step 2 & 3:
+surveys_wide <- pivot_wider(surveys2, names_from = "plot_type", values_from = "mean_hindfoot")
+surveys_wide
+
+surveys_wide %>% 
+  arrange(desc(Control))
+
+surveys_wide
+
+# What if we wanted to reverse this back into the longer version of what we made before
+?pivot_longer
+surveys_reverse <- surveys_wide %>%
+  pivot_longer(cols = c(Control:`Spectab exclosure`),
+               names_to = "plot_type",
+               values_to = "mean_hindfoot")
+# cols:  which columns I want to pivot
+# names_to: takes the column name and puts them into a column. What do you want to name the column of column names?
+# values_to: takes the values from each of these columns cells. What do you want to name the column of cell values?
